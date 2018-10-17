4d72bcb1b534f211974346ea72a433d01e3c31aa
Alyssa <fusaroalyssa@gmail.com>
2018-10-17 14:31:54 -0400

    get user by name, add exercise

--- server/controller.js
+++ server/controller.js
@@ -1,25 +1,43 @@
 const express = require('express');
-const {UserData, User} = require('./model');
+const {UserData, User, Exercise} = require('./model');
 
 const userData = new UserData();
 
 const app = express.Router();
 
 
-
+//Home Page
 app.get("/", function(req, res){
     res.send("Home Page");
 })
 
+//add a user to database
 app.post("/users", function(req, res){
     const user = new User(req.body.name, req.body.email, req.body.password)
     userData.addUser(user);
     res.send(userData.user + " user added");
 })
 
+//display all users info in database
 app.get("/users", function(req, res){
     res.send(userData.users);
 })
+
+
+//get a user
+app.get("/users/:name", (req, res) => {
+    res.send(userData.getUser(req.params.name));
+    //console.log(userData.getUser(req.params.name));
+})
+
+
+//add an exercise performed by a user
+app.post("/users/exercise/:name", (req, res) => {
+    const user = userData.getUser(req.params.name);
+    const exercise = new Exercise(req.body.exName, req.body.hour, req.body.min, req.body.sec);
+    res.send(user.addExercise(exercise));
+})
+
 
 
 module.exports = app;
--- server/model.js
+++ server/model.js
@@ -1,9 +1,8 @@
 class UserData
 {
-    constructor(users)
+    constructor()
     {
         this.users = [];
-        this.exercises = [];
     }
 
     addUser(user)
@@ -11,19 +10,58 @@ class UserData
         this.users.push(user);
     }
 
+    getUser(name)
+    {
+        var index = this.users.findIndex(user => user.name === name);
+        return this.users[index];
+        /*
+       var i;
+       for(i = 0; i< this.users.length; i++)
+       {
+           if(name === this.users[i].name)
+           {
+               return this.users[i];
+           }
+       }*/
+    }
 }
 
 class User
 {
+    
     constructor(name, email, password)
     {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.exercises = [];
+        this.friendsList = [];
     } 
+
+    addExercise(exercise)
+    {
+        this.exercises.push(exercise);
+    }
+
+    addFriend(friend)
+    {
+        this.friendsList.push(friend);
+    }
 }
 
 
+class Exercise
+{
+    constructor(exName, hour, min, sec)
+    {
+        this.exName = exName;
+        this.hour = hour;
+        this.min = min;
+        this.sec = sec;
+        this.videoLink;
+    }
+}
+
 module.exports = {
-    UserData, User
+    UserData, User, Exercise
 }
